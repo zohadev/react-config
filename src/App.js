dcf4cdce88058eb9a150ea8314102617e6a6b542
zohad <zohadsikder164@gmail.com>
2021-11-23 17:22:04 +0600

    2

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, guys it's my 1st react app
+          Hello, guys it's my 1st react app and it's updated
         </p>
+
       </header>
     </div>
   );
